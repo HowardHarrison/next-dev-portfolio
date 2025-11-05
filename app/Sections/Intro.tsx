07fe757f2c6ab883da2302dd49975a1cd64cfcb0
HowardHarrison <peterthureain@gmail.com>
2025-11-05 23:07:00 +0630

    [Remove] unused hooks in Intro

--- app/Sections/Intro.tsx
+++ app/Sections/Intro.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import { FaDownload } from "react-icons/fa";
 import Starfield from "../animation/StarField";
